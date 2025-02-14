a1e243bc708ec3246e48d049b72d43050794af93
Teuku Rizqy Ramadhan <kikidotid@gmail.com>
2025-02-14 07:48:28 +0700

    port 80

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // Agar bisa diakses dari IP lain
+    port: 80, // Mengubah port menjadi 80
+    strictPort: true, // Agar tidak pindah ke port lain jika 80 dipakai
+  },
+});
