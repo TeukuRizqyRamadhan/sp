8a21fa27f63e036ad086f5d95e33cf9b7bb96589
Teuku Rizqy Ramadhan <kikidotid@gmail.com>
2025-02-14 07:35:04 +0700

    pc huawei

--- src/pages/ExportData.tsx
+++ src/pages/ExportData.tsx
@@ -2,27 +2,28 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const ExportData = () => {
+  const navigate = useNavigate();
+  const checkToken = () => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/");
+    }
+  };
 
-    const navigate = useNavigate();
-    const checkToken = () => {
-        const token = localStorage.getItem("token");
-        if (!token) {
-            navigate("/");
-        }
-    };
-
-    useEffect(() => {
-        checkToken();
-    }, []);
-    return (
-        <div className="p-4 border rounded">
-            <h2 className="text-xl font-bold">Export Data Surat Pembinaan</h2>
-            <p className="text-gray-600 mt-2">Silahkan Pilih Rentang Tanggal / Bulan / Tahun </p>
-            <button className="bg-blue-500 text-white px-4 py-2 mt-2">
-                Export Data
-            </button>
-        </div>
-    );
+  useEffect(() => {
+    checkToken();
+  }, []);
+  return (
+    <div className="p-4 border rounded">
+      <h2 className="text-xl font-bold">Export Data Surat Pembinaan</h2>
+      <p className="text-gray-600 mt-2">
+        Silahkan Pilih Rentang Tanggal / Bulan / Tahun{" "}
+      </p>
+      <button className="bg-blue-500 text-white px-4 py-2 mt-2">
+        Export Data
+      </button>
+    </div>
+  );
 };
 
 export default ExportData;
